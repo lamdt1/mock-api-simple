cc57f4172ef6c7a24e2a18353728b8a407872c8e
LamDT <dinhtienlam.spkt@gmail.com>
2020-03-30 17:20:42 +0700

    init simple mock api server

---
+++ App.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var multer = require('multer');
+var app = express();
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage: storage }).single('userFile');
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/upload', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+    res.end("File is uploaded");
+  });
+});
+
+app.post('/api/register', function (req, res) {
+  setTimeout(() => {
+    return res.end({ "message": { "shared_token": "ZsPitF57" } })
+  }, 1000)
+});
+
+app.listen(3000, function () {
+  console.log("Working on port 3000");
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+==================================================
+Mock API With Node
+==================================================
+
+# Install dependencies
+
+    ```npm install```
+# Start mock api server
+    
+    ```npm start```
+# Preview
+
+    Access http://localhost:3000
